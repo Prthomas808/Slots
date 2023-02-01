3373a20c97178be45b59ea8763008317a9e3984a
Tico <121649539+Ticothomas8@users.noreply.github.com>
2023-02-01 06:52:20 -0500

    Initial Commit

---
+++ Slots/SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotsApp.swift
+//  Slots
+//
+//  Created by Tico Thomas on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
